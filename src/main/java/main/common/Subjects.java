9bdb8a01da2c72f8c903d57c991674caf6a2425f
Andrei Kolotilo <andrei.kolotilo@netcracker.com>
2019-12-03 13:47:40 +0300

    add all files

--- src/main/java/main/common/Subjects.java
+++ src/main/java/main/common/Subjects.java
@@ -1,4 +1,12 @@
 package main.common;
 
+import com.sun.org.glassfish.gmbal.Description;
+
 public enum Subjects {
+    Physics,
+    Math,
+    InformationScience,
+    English,
+    Biology,
+    History
 }
--- src/main/java/main/student/Student.java
+++ src/main/java/main/student/Student.java
@@ -1,4 +1,13 @@
 package main.student;
 
 public class Student {
+    private String name, surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
