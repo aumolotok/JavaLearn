5f9aa382665c07cf141f72a91fc5b00517120715
aumolotok <aumolotok@mail.ru>
2019-12-03 22:14:54 +0300

    modify Student Student.java

--- src/main/java/main/student/Student.java
+++ src/main/java/main/student/Student.java
@@ -1,5 +1,9 @@
 package main.student;
 
+import main.common.Subjects;
+
+import java.util.Map;
+
 public class Student {
     private String name, surname;
 
@@ -9,5 +13,17 @@ public class Student {
 
     public String getSurname() {
         return surname;
+    }
+
+    private Map<Subjects, Integer> subjectsMarks;
+
+    public Map<Subjects, Integer> getSubjectsMarks() {
+        return subjectsMarks;
+    }
+
+    public Student(String name, String surname, Map<Subjects, Integer> marks){
+        this.name = name;
+        this.surname = surname;
+        this.subjectsMarks = marks;
     }
 }
