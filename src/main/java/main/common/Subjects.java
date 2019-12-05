d7acd5d5a1c815ed6556da2c924b14c35ffb9660
aumolotok <aumolotok@mail.ru>
2019-12-05 23:21:35 +0300

    Add Subjects enum

--- src/main/java/main/common/Subjects.java
+++ src/main/java/main/common/Subjects.java
@@ -1,9 +1,35 @@
 package main.common;
+
+import jdk.jfr.Description;
+
 public enum Subjects {
-    Physics,
-    Math,
-    InformationScience,
-    English,
-    Biology,
-    History
+    Physics("Физика", SubjectType.Tech),
+    Math("Математика", SubjectType.Tech),
+    InformationScience("Информатика", SubjectType.Tech),
+    English("Английский", SubjectType.Social),
+    Biology("Биология", SubjectType.Natural),
+    History("История", SubjectType.Social);
+
+    private String translation;
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    private SubjectType type;
+
+    public SubjectType getType(){
+        return type;
+    }
+
+    private Subjects(String translation, SubjectType type){
+        this.translation = translation;
+        this.type = type;
+    }
+
+    public enum SubjectType {
+        Tech,
+        Social,
+        Natural
+    }
 }
