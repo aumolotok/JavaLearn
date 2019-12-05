6c7937558a1ff8379260a8fa151041061fa393dd
aumolotok <aumolotok@mail.ru>
2019-12-06 00:25:50 +0300

    Add parser Parser.java and prototyped it

--- src/main/java/main/parser/Parser.java
+++ src/main/java/main/parser/Parser.java
@@ -1,19 +1,32 @@
 package main.parser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 public class Parser {
 
-    private static String read(String fileName){
-        String result = new String();
-        try {
-           result = new String(Files.readAllBytes(Paths.get(fileName)));
-        }
-        catch(IOException exception) {
-            System.out.println("Error while reading file");
-        }
-        return result;
+    private String fileName;
+    private String pattern = "((Ф|ф)амилия)(\\D+)((И|и)мя)(\\D+)((П|п)редмет)(\\D+)((О|о)ценка)";
+
+    private List<String> read() throws IOException {
+        List<String> lines =  Files.readAllLines(Paths.get(fileName));
+        return lines;
+    }
+
+    public void Parse() throws IOException {
+        List<String> lines = read().stream().filter( x -> !x.matches(pattern)).collect(Collectors.toList());
+    }
+
+    //
+
+    public Parser(String fileName){
+        this.fileName = fileName;
     }
 }
