e274c9e53294cc159462b708947f6675fb00320d
aumolotok <aumolotok@mail.ru>
2019-12-06 00:26:46 +0300

    Add Some methods to Main method

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
-public class Main {
-    public static void main(String[] args){
+import main.parser.Parser;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Parser parser = new Parser("target.txt");
+
+        parser.Parse();
     }
 }
