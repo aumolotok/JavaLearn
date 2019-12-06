3a1ad501010c4ae2c75529913419bcfbb97cc766
aumolotok <aumolotok@mail.ru>
2019-12-07 00:59:32 +0300

    Continue developing of Parse method

--- src/main/java/main/parser/Parser.java
+++ src/main/java/main/parser/Parser.java
@@ -1,11 +1,15 @@
 package main.parser;
 
+import main.common.Subjects;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
@@ -22,11 +26,29 @@ public class Parser {
 
     public void Parse() throws IOException {
         List<String> lines = read().stream().filter( x -> !x.matches(pattern)).collect(Collectors.toList());
+
+        List<List<String>> characteristics = new ArrayList<List<String>>();
+
+        for(String line : lines){
+            List<String> studentChars = Arrays.asList(line.split("\\s*\\|\\s*"));
+            if(studentChars.size() == 4) {
+                characteristics.add(studentChars);
+            }
+        }
+
+        List<String> surnamesWithNames =  characteristics.stream().map( x -> x.get(0)+x.get(1)).distinct().collect(Collectors.toList());
+
+        for(String person : surnamesWithNames){
+
+        }
+
     }
 
-    //
+
 
     public Parser(String fileName){
         this.fileName = fileName;
     }
+
+
 }
