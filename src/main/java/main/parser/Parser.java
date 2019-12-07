5e687eb3b904174c72260a53b87ee4da1157223f
aumolotok <aumolotok@mail.ru>
2019-12-08 00:09:40 +0300

    Rename refactoring

--- src/main/java/main/parser/Parser.java
+++ src/main/java/main/parser/Parser.java
@@ -2,16 +2,12 @@ package main.parser;
 
 import main.common.Subjects;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 public class Parser {
@@ -28,30 +24,32 @@ public class Parser {
     public void Parse() throws IOException {
         List<String> lines = read().stream().filter( x -> !x.matches(pattern)).collect(Collectors.toList());
 
-        List<PersonMark> marks = new ArrayList<PersonMark>();
+        List<PersonSubjectMark> marks = new ArrayList<PersonSubjectMark>();
 
         for(String line : lines){
             if(!validator.isValidPersonString(line)){
                 continue;
             }
             List<String> studentChars = Arrays.asList(line.split("\\s*\\|\\s*"));
-            marks.add(new PersonMark(studentChars));
+            marks.add(new PersonSubjectMark(studentChars));
         }
     }
 
+    public void combineMarksForStudents(List<PersonSubjectMark> p){
 
+    }
 
     public Parser(String fileName){
         this.fileName = fileName;
     }
 
-    private class PersonMark{
+    private class PersonSubjectMark {
         public String Name;
         public String Surname;
         public Subjects Subject;
         public int Mark;
 
-        public PersonMark(List<String> personCharacteristics){
+        public PersonSubjectMark(List<String> personCharacteristics){
             this.Surname = personCharacteristics.get(0);
             this.Name = personCharacteristics.get(1);
             this.Subject = Subjects.getByTitle(personCharacteristics.get(2));
