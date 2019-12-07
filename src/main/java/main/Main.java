04b5cd6b2ff2517018f92b059021194d26b8cb0f
aumolotok <aumolotok@mail.ru>
2019-12-08 00:59:42 +0300

    Add steps to Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,14 @@
 package main;
 
 import main.parser.Parser;
+import main.student.Student;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Parser parser = new Parser("target.txt");
-
-        parser.Parse();
+        List<Student> students =  parser.Parse();
     }
 }
