7a8c49c439a249b7ed4a918d0ad1ab576a071789
aumolotok <aumolotok@mail.ru>
2019-12-08 20:34:12 +0300

    Add new Utile method

--- src/main/java/main/utils/NumberUtils.java
+++ src/main/java/main/utils/NumberUtils.java
@@ -1,5 +1,8 @@
 package main.utils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class NumberUtils {
     public static boolean isStringADigit(String string) {
         try {
@@ -8,5 +11,13 @@ public class NumberUtils {
             return false;
         }
         return true;
+    }
+
+    public static double roundDouble(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
