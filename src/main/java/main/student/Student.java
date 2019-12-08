3abb7bcd1bc6e0cc99649fe4c166fa998e416bdb
aumolotok <aumolotok@mail.ru>
2019-12-08 20:36:25 +0300

    Add getAverageForSubjectType and getFullNamemethods to Student.java

--- src/main/java/main/student/Student.java
+++ src/main/java/main/student/Student.java
@@ -1,8 +1,12 @@
 package main.student;
 
 import main.common.Subjects;
+import main.utils.NumberUtils;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Student {
     private String name, surname;
@@ -19,6 +23,18 @@ public class Student {
 
     public Map<Subjects, Integer> getSubjectsMarks() {
         return subjectsMarks;
+    }
+
+    public double getAverageForSubjectType(Subjects.SubjectType type){
+        List<Subjects> techSubjects = Subjects.getSubjectsByType(type);
+
+        Double unroundValue = techSubjects.stream().map(x -> subjectsMarks.get(x)).collect(Collectors.averagingDouble(num -> num));
+
+        return NumberUtils.roundDouble(unroundValue, 3);
+    }
+
+    public String getFullName(){
+        return name + " " + surname;
     }
 
     public Student(String name, String surname, Map<Subjects, Integer> marks){
