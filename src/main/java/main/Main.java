a4c7b16c3ce5b01eb30c599d4c5abd5f9fd2fc6c
aumolotok <aumolotok@mail.ru>
2019-12-08 20:37:48 +0300

    Add steps to Main method

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,27 @@
 package main;
 
+import main.common.Subjects;
 import main.parser.Parser;
 import main.student.Student;
-
+import main.student.comparators.AverageComparator;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Parser parser = new Parser("target.txt");
         List<Student> students =  parser.Parse();
+
+        List<Student> sortedByTechAverage = students.stream()
+                .sorted(new AverageComparator(Subjects.SubjectType.Tech))
+                .collect(Collectors.toList());
+
+//        sortedByTechAverage.stream()
+//                .forEach( st ->  System.out.println(st.getFullName() + " averTech " + st.getAverageForSubjectType(Subjects.SubjectType.Tech) ));
+
     }
 }
+
+
+
